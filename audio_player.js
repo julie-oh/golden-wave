42b4f411c0bb20a4c7410995e914f4e633d11064
julie-oh <yj_oh@naver.com>
2017-12-21 15:49:27 +0900

    로컬 파일을 업로드하여 음원 재생 기능 구현.

--- audio_player.js
+++ audio_player.js
@@ -1,23 +1,35 @@
+/**
+ * Audio Player class for audio playback.
+ */
 class AudioPlayer {
   constructor() {
     this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
     this.dst = this.audioCtx.destination;
     this.bufferSource = this.audioCtx.createBufferSource();
+    this.addLoadEventHandler('audiofile');
   }
 
+  /**
+   * Add load event handler for file input.
+   * It plays the audio file locally loaded by the user.
+   *
+   * Arg:
+   *  fileInputTag: string indicating id of file input tag
+   */
   addLoadEventHandler(fileInputTag) {
     const fileInputElem = document.getElementById(fileInputTag);
     const fileReader = new FileReader();
-    fileInputElem.addEventListener("change", function() {
-      fileReader.onload = function(e) {
-        console.log("debug");
-        this.audioCtx.decodeAudioData(e.target.result, function(buffer) {
+    fileInputElem.addEventListener('change', () => {
+      // reads the file as ArrayBuffer for audio context to decode
+      fileReader.onload = (e) => {
+        this.audioCtx.decodeAudioData(e.target.result).then((buffer) => {
           this.bufferSource.buffer = buffer;
           this.bufferSource.connect(this.dst);
-          this.bufferSource.noteOn(0);
+          this.bufferSource.start(0);  // start playing right away
         });
-        fileReader.readAsArrayBuffer(this.files[0]);
       }
+
+      fileReader.readAsArrayBuffer(fileInputElem.files[0]);
     }, false);
   }
 }
